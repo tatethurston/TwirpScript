3677893810806e4b73aaafcbb6f960b6a7390233
Tate <tate@transcriptic.com>
2021-11-16 16:14:38 -0800

    fix issue #7

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync } from "fs";
 import {
   CodeGeneratorRequest,
   CodeGeneratorResponse,
@@ -45,4 +45,4 @@ request.getProtoFileList().forEach((fileDescriptorProto) => {
   );
 });
 
-writeFileSync(process.stdout.fd, response.serializeBinary());
+process.stdout.write(response.serializeBinary());

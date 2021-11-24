201a6f0a3c291bd9b3889db8c11cf24d05c7c2f2
Tate <tate@transcriptic.com>
2021-11-24 12:54:26 -0800

    add clientcompat test

---
+++ twirp-clientcompat/src/test.ts
@@ -0,0 +1,12 @@
+import { describe, it } from "@jest/globals";
+import { spawnSync } from "child_process";
+
+describe("Twirp Client Compatabilitiy Test", () => {
+  it("passes", () => {
+    const child = spawnSync(`./clientcompat -client=./test`, {
+      encoding: "utf8",
+      shell: true,
+    });
+    expect(child.stdout).toMatchSnapshot();
+  });
+});

12908e31c5a606caa90e7e6325f167ec2e59d01d
Tate <tate@transcriptic.com>
2021-12-27 00:35:11 -0800

    refactor client and client tests

---
+++ src/runtime/client/mocks.ts
@@ -0,0 +1,10 @@
+import { RpcTransportResponse } from ".";
+
+export const mockRpcTransportResponse: RpcTransportResponse = {
+  arrayBuffer: () => Promise.resolve(Buffer.from([])),
+  json: () => Promise.resolve({}),
+  ok: true,
+  status: 500,
+  text: () => Promise.resolve(""),
+  headers: { get: () => null },
+};

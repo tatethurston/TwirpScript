3896800179c9de2f24ee5b548a45b5597587bb3c
Tate Thurston <tatethurston@gmail.com>
2022-01-08 19:43:27 -0800

    refactor e2e:tests (#80)
    Co-authored-by: Tate <tate@transcriptic.com>

--- src/test-protos/gen-test-input.sh
+++ src/test-protos/gen-test-input.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Run from src/test-protos to regnerate test-input-typescript and test-input-javascript.
+#
+# This only needs to be done when:
+# - new or changed .proto files
+# - new or changed compilar parameters ( --twirpscript_out=<PARAMETER> )
 
 protoc \
   --plugin=protoc-gen-twirpscript=../../dist/test-protos/gen-test-input.js \

ca2f56d92cb2d3e65ccac9a394903446598fea45
Tate Thurston <tatethurston@gmail.com>
2022-11-18 12:29:10 -0800

    use protoscript compiler

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,22 +1,4 @@
 #!/usr/bin/env node
+import { compiler } from "protoscript/plugin";
 import { compile } from "./compile.js";
-
-function readStream(stream: NodeJS.ReadStream): Promise<Uint8Array> {
-  return new Promise((resolve) => {
-    const chunks: Buffer[] = [];
-    stream.on("readable", () => {
-      let chunk: Buffer;
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      while ((chunk = process.stdin.read()) !== null) {
-        chunks.push(chunk);
-      }
-    });
-    stream.on("end", () => {
-      resolve(Buffer.concat(chunks));
-    });
-  });
-}
-
-const input = await readStream(process.stdin);
-const response = compile(input);
-process.stdout.write(response.serializeBinary());
+await compiler(compile);

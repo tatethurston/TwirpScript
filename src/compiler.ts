b370e8b64ee3c395975a0348f3752f9ec47cc2a5
Tate Thurston <tatethurston@gmail.com>
2022-11-18 12:13:29 -0800

    v0.0.65 (#206) (#209)

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
-import { readFileSync } from "fs";
 import { compile } from "./compile.js";
 
-const input = readFileSync(0);
+function readStream(stream: NodeJS.ReadStream): Promise<Uint8Array> {
+  return new Promise((resolve) => {
+    const chunks: Buffer[] = [];
+    stream.on("readable", () => {
+      let chunk: Buffer;
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      while ((chunk = process.stdin.read()) !== null) {
+        chunks.push(chunk);
+      }
+    });
+    stream.on("end", () => {
+      resolve(Buffer.concat(chunks));
+    });
+  });
+}
+
+const input = await readStream(process.stdin);
 const response = compile(input);
 process.stdout.write(response.serializeBinary());
